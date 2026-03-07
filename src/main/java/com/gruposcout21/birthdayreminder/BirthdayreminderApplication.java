3a2037aebe95387006029de7fab6dabacb763f59
Julio Gomez <jufergom@hotmail.com>
2026-03-06 22:47:24 -0600

    first commit

---
+++ src/main/java/com/gruposcout21/birthdayreminder/BirthdayreminderApplication.java
@@ -0,0 +1,13 @@
+package com.gruposcout21.birthdayreminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirthdayreminderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirthdayreminderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gruposcout21/birthdayreminder/BirthdayreminderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gruposcout21.birthdayreminder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirthdayreminderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
